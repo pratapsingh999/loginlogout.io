a73aa374ed0a559b4380be4ff71a92df5d7a6e52
pratapsingh999 <108220183+pratapsingh999@users.noreply.github.com>
2023-07-05 19:19:14 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  // const { loginWithRedirect } = useAuth0();
+  const {loginWithRedirect, logout ,isAuthenticated,User} = useAuth0();
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>well come to social media icons</h1>
     </div>
+    
+   <li>
+      {isAuthenticated && <p>{User.name}</p>}
+    </li>
+    {
+      isAuthenticated ? ( <li>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      </li>):(  <li>
+      <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button>
+      </li>)
+     } 
+   
+    
+
+
+
+    <ul>
+      <li>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      </li>
+      <li>
+      <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button>
+      </li>
+    </ul>
+
+
+    {/* <div><button onClick={() => loginWithRedirect()}>Log In</button></div> */}
+    {/* <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button> */}
+    </>
   );
 }
 
